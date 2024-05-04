b237bf8147fef4f762b1eb14a5bc27db668b7a31
Martina Vokáčová <90708462+espressomartiny@users.noreply.github.com>
2024-05-04 13:10:43 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: "/pseudorator/",
+  base: "/",
   root: './src',
   publicDir: '../public',
   server: {
