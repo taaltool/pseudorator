edc5bd5a8d4c77d04c5fc64f539c27e93dc4a6de
Martina Vokáčová <martinavokacova@gmail.com>
2024-05-04 17:31:33 +0200

    reverted logic for early exit

--- src/components/BigramWeigher/index.jsx
+++ src/components/BigramWeigher/index.jsx
@@ -1,26 +1,31 @@
-const BigramWeigher = (bigrams, position) => {
+const BigramWeigher = (bigrams, lastLetter, position) => {
   const validBigrams = bigrams.filter(
     (bigram) =>
       bigram &&
+      bigram.bigram[0] === lastLetter && 
       bigram.sum_log_freq_pos &&
       bigram.sum_log_freq_pos[position.toString()]
   );
-  if (validBigrams.length > 0) {
-    const weights = validBigrams.map((bigram) => ({
-      bigram: bigram.bigram,
-      weight: bigram.sum_log_freq_pos[position.toString()],
-    }));
-    const totalWeight = weights.reduce((acc, next) => acc + next.weight, 0);
-    const random = Math.random() * totalWeight;
-    let sum = 0;
+  if (validBigrams.length === 0) {
+    return null;
+  }
 
-    for (const { bigram, weight } of weights) {
-      sum += weight;
-      if (random <= sum) {
-        return bigram;
-      }
+  const weights = validBigrams.map((bigram) => ({
+    bigram: bigram.bigram,
+    weight: bigram.sum_log_freq_pos[position.toString()],
+  }));
+  const totalWeight = weights.reduce((acc, next) => acc + next.weight, 0);
+  const random = Math.random() * totalWeight;
+  let sum = 0;
+
+  for (const { bigram, weight } of weights) {
+    sum += weight;
+    if (random <= sum) {
+      return bigram;
     }
   }
+  
+  console.error(`no bigram selected ${lastLetter}, ${position}, ${sum}, ${random}`)
   return null;
 };
 
