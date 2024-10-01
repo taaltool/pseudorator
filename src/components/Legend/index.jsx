e52981568767eaba7c208aa634e7f315dd6d26e8
Martina Vokáčová <martinavokacova@gmail.com>
2024-10-01 19:14:03 +0200

    use innerWidth as state

--- src/components/Legend/index.jsx
+++ src/components/Legend/index.jsx
@@ -3,19 +3,18 @@ import React, { useState, useEffect } from 'react';
 const Legend = () => {
   const [isVisible, setIsVisible] = useState(false);
   const [leftOffset, setLeftOffset] = useState(0);
-  const [isActive, setIsActive] = useState(false); 
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth); 
 
   const toggleLegend = () => {
     setIsVisible(!isVisible);
-    setIsActive(!isVisible); 
   };
 
   useEffect(() => {
     const handleResize = () => {
+      setWindowWidth(window.innerWidth); 
       const container = document.querySelector('.container');
       if (container) {
         const containerRect = container.getBoundingClientRect();
-        
         
         if (window.innerWidth > 500) {
           setLeftOffset(containerRect.right + 20);
@@ -26,7 +25,7 @@ const Legend = () => {
     };
 
     handleResize(); 
-    window.addEventListener('resize', handleResize);
+    window.addEventListener('resize', handleResize); 
 
     return () => {
       window.removeEventListener('resize', handleResize);
@@ -37,10 +36,7 @@ const Legend = () => {
     <>
       <button className="legendButton" onClick={toggleLegend} style={{ cursor: 'pointer' }}>?</button>
       {isVisible && (
-        <div
-          className={`legendPopup ${isActive ? 'active' : ''}`} 
-          style={{ left: window.innerWidth > 500 ? `${leftOffset}px` : '0' }}
-        >
+        <div className="legendPopup" style={{ left: windowWidth > 500 ? `${leftOffset}px` : '0' }}>
           <h3>What is this tool?</h3>
           <p>This tool is ulalalalalal tralalalala (the legend to be completed)</p>
           <p>This tool is ulalalalalal tralalalala (the legend to be completed)</p>
