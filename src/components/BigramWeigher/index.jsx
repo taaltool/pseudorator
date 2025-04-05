541248413a240523368c449ba23c3ffbcfd5989e
haf-cz <josefkafka1@gmail.com>
2025-04-05 23:09:53 +0200

    extract and fix functionality

--- src/components/BigramWeigher/index.jsx
+++ src/components/BigramWeigher/index.jsx
@@ -1,3 +1,5 @@
+import { chooseRandomBigram } from './helpers.js'
+
 const getBigram = (bigrams, character, position, characterPosition) => {
   const validBigrams = bigrams.filter(
     (bigram) =>
@@ -6,13 +8,13 @@ const getBigram = (bigrams, character, position, characterPosition) => {
       bigram.sum_log_freq_pos &&
       bigram.sum_log_freq_pos[position.toString()]
   );
-  
+
   if (validBigrams.length === 0) { 
     console.error(`no bigram selected. Character: '${character}' on position ${position}, characterPosition ${characterPosition}`)
     return null;
   }
 
-  return chooseRandomBigram(validBigrams).bigram;
+  return chooseRandomBigram(validBigrams, position).bigram;
 }
 
 const getRandomBigram = (bigrams, position) => {
@@ -29,7 +31,7 @@ const getRandomBigram = (bigrams, position) => {
     return null;
   }
 
-  return chooseRandomBigram(validBigrams).bigram
+  return chooseRandomBigram(validBigrams, position).bigram
 }
 
 const getY_X = (bigrams, left, right, position) => {
@@ -64,31 +66,7 @@ const getY_X = (bigrams, left, right, position) => {
     return null;
   }
 
-  return chooseRandomBigram(validBigrams).bigram[1]
-}
-
-const chooseRandomBigram = (bigrams) => {
-  const randomIndex = Math.floor(Math.random() * bigrams.length)
-  return bigrams[randomIndex]
-  /*
-  const weights = bigrams.map((bigram) => ({
-    bigram: bigram.bigram,
-    weight: bigram.sum_log_freq_pos[position.toString()],
-  }));
-  const totalWeight = weights.reduce((acc, next) => acc + next.weight, 0);
-  const random = Math.random() * totalWeight;
-  let sum = 0;
-
-  for (const { bigram, weight } of weights) {
-    sum += weight;
-    if (random <= sum) {
-      return bigram;
-    }
-  }
-  
-  console.error(`no bigram selected ${character}, ${position}, ${sum}, ${random}`)
-  return null;
-   */
+  return chooseRandomBigram(validBigrams, position).bigram[1]
 }
 
 export {getBigram, getRandomBigram, getY_X}
