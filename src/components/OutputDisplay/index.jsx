41550327aba860c45ec4c7b4b54993848565b34a
haf-cz <josefkafka1@gmail.com>
2025-04-06 02:09:19 +0200

    refactor: extracting common IPA to constants

--- src/components/OutputDisplay/index.jsx
+++ src/components/OutputDisplay/index.jsx
@@ -1,31 +1,8 @@
 import React, { useState } from "react";
+import { ipaCharacters } from '../../helpers/constants.js'
 
 const OutputDisplay = ({ words, isPhonotactic }) => {
   const [showIpaCharacters, setShowIpaCharacters] = useState(false);
-  const ipaCharacters = [
-    { ipa: "aː", grapheme: "á" },
-    { ipa: "a͡u", grapheme: "au" },
-    { ipa: "ʦ", grapheme: "c" },
-    { ipa: "ʧ", grapheme: "č" },
-    { ipa: "ɟ", grapheme: "ď" },
-    { ipa: "ʣ", grapheme: "dz" },
-    { ipa: "ʤ", grapheme: "dž" },
-    { ipa: "ɛ", grapheme: "e" },
-    { ipa: "ɛː", grapheme: "é" },
-    { ipa: "ɛ͡u", grapheme: "eu" },
-    { ipa: "ɦ", grapheme: "h" },
-    { ipa: "x", grapheme: "ch" },
-    { ipa: "ɪ", grapheme: "i" },
-    { ipa: "iː", grapheme: "í" },
-    { ipa: "ɲ", grapheme: "ň" },
-    { ipa: "oː", grapheme: "ó" },
-    { ipa: "o͡u", grapheme: "ou" },
-    { ipa: "r̝", grapheme: "ř" },
-    { ipa: "ʃ", grapheme: "š" },
-    { ipa: "c", grapheme: "ť" }, 
-    { ipa: "uː", grapheme: "ú" },
-    { ipa: "ʒ", grapheme: "ž" },
-];
 
   const toggleIpaCharacters = () => {
     setShowIpaCharacters(!showIpaCharacters);
--- src/helpers/constants.js
+++ src/helpers/constants.js
@@ -1,2 +1,30 @@
 export const ORTHO = "orthotactic"
 export const PHONO = "phonotactic"
+
+/** @constant other valid characters for phonotactic bigrams
+    @type {Array}
+*/
+export const ipaCharacters = [
+    { ipa: "aː", grapheme: "á" },
+    { ipa: "a͡u", grapheme: "au" },
+    { ipa: "ʦ", grapheme: "c" },
+    { ipa: "ʧ", grapheme: "č" },
+    { ipa: "ɟ", grapheme: "ď" },
+    { ipa: "ʣ", grapheme: "dz" },
+    { ipa: "ʤ", grapheme: "dž" },
+    { ipa: "ɛ", grapheme: "e" },
+    { ipa: "ɛː", grapheme: "é" },
+    { ipa: "ɛ͡u", grapheme: "eu" },
+    { ipa: "ɦ", grapheme: "h" },
+    { ipa: "x", grapheme: "ch" },
+    { ipa: "ɪ", grapheme: "i" },
+    { ipa: "iː", grapheme: "í" },
+    { ipa: "ɲ", grapheme: "ň" },
+    { ipa: "oː", grapheme: "ó" },
+    { ipa: "o͡u", grapheme: "ou" },
+    { ipa: "r̝", grapheme: "ř" },
+    { ipa: "ʃ", grapheme: "š" },
+    { ipa: "c", grapheme: "ť" }, 
+    { ipa: "uː", grapheme: "ú" },
+    { ipa: "ʒ", grapheme: "ž" },
+];
